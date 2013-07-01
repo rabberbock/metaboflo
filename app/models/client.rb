1b98b00b35c441adf15eaf46e92c8f48bcba81c4
Raffi Abberbock <rabberbock@gmail.com>
2013-06-30 22:01:40 -0400

    client changes

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,7 @@
 class Client < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, 
           :lockable,  :registerable
-  #
+ 
   # Setup accessible attributes
   # attr_accessible :email, :password, :password_confirmation, :name, :site_id, :rank
   attr_protected :current_password
@@ -9,7 +9,7 @@ class Client < ActiveRecord::Base
   has_many :samples
   has_many :sample_manifests
   
-  
+  #validates :name, :presence => true
   
   def to_s
     self.email
